4e815db12cf7fd2c6166aaa367077eeeab297029
Hamilcar Steven B. Chanjueco, DM <64169437+skaperduo@users.noreply.github.com>
2020-05-02 15:40:16 +0800

    Update settings.py

--- aciWebsite/settings.py
+++ aciWebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '&+^v6$7v+f44q_+t1yfep#p0dnoi1o0zl0x#*908@k$#0csslq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['agusancolleges.herokuapp.com']
 # ALLOWED_HOSTS = []
