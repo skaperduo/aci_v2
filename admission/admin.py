aedb842de1ebe895c760f9c7b2e510e342a31620
Steven Chanjueco <hsb.chanjueco@gmail.com>
2020-05-15 18:55:59 +0800

    added search field in django admin

--- admission/admin.py
+++ admission/admin.py
@@ -9,6 +9,7 @@ from .models import Programs, SHSstrands, PreferredShift, YearLevel, GradeLevel,
 class SPIAdmin(admin.ModelAdmin):
     ordering = ['date_registered']
     list_per_page = 10
+    search_fields = ('last_name', 'first_name', 'middle_name', 'stud_reference_no')
     list_display = ('last_name', 'first_name', 'middle_name', 'stud_program', 'connectivity', 'date_registered',
                     'mobile_number', 'stud_reference_no')
     list_filter = ('stud_program', 'connectivity', 'date_registered')
@@ -17,6 +18,7 @@ class SPIAdmin(admin.ModelAdmin):
 class Grad_SPIAdmin(admin.ModelAdmin):
     ordering = ['grad_date_registered']
     list_per_page = 10
+    search_fields = ('grad_last_name', 'grad_first_name', 'grad_middle_name', 'grad_stud_reference_no')
     list_display = ('grad_last_name', 'grad_first_name', 'grad_middle_name', 'grad_stud_program', 'connectivity',
                     'grad_date_registered', 'grad_mobile_number', 'grad_stud_reference_no')
     list_filter = ('grad_stud_program', 'connectivity', 'grad_date_registered')
@@ -25,6 +27,7 @@ class Grad_SPIAdmin(admin.ModelAdmin):
 class SHS_SPIAdmin(admin.ModelAdmin):
     ordering = ['shs_date_registered']
     list_per_page = 10
+    search_fields = ('shs_last_name', 'shs_first_name', 'shs_middle_name', 'shs_reference_no')
     list_display = ('shs_last_name', 'shs_first_name', 'shs_middle_name', 'shs_stud_grade_level', 'shs_stud_strand',
                     'connectivity', 'shs_date_registered', 'shs_mobile_number', 'shs_reference_no')
     list_filter = ('shs_stud_grade_level', 'shs_stud_strand', 'connectivity', 'shs_date_registered')
@@ -33,6 +36,7 @@ class SHS_SPIAdmin(admin.ModelAdmin):
 class JHS_SPIAdmin(admin.ModelAdmin):
     ordering = ['jhs_date_registered']
     list_per_page = 10
+    search_fields = ('jhs_last_name', 'jhs_first_name', 'jhs_middle_name', 'jhs_reference_no')
     list_display = ('jhs_last_name', 'jhs_first_name', 'jhs_middle_name', 'jhs_stud_grade_level',
                     'connectivity', 'jhs_date_registered', 'jhs_mobile_number', 'jhs_reference_no')
     list_filter = ('jhs_stud_grade_level', 'connectivity', 'jhs_date_registered')
@@ -41,6 +45,7 @@ class JHS_SPIAdmin(admin.ModelAdmin):
 class E_SPIAdmin(admin.ModelAdmin):
     ordering = ['elementary_date_registered']
     list_per_page = 10
+    search_fields = ('elementary_last_name', 'elementary_first_name', 'elementary_middle_name', 'elem_reference_no')
     list_display = ('elementary_last_name', 'elementary_first_name', 'elementary_middle_name',
                     'elementary_stud_grade_level', 'connectivity', 'elementary_date_registered',
                     'elementary_mobile_number', 'elem_reference_no')
