6fcc0f59e08662da7c333128197f62d836d2f3ad
Steven Chanjueco <hsb.chanjueco@gmail.com>
2020-05-16 21:15:04 +0800

    some settings changes

--- aciWebsite/settings.py
+++ aciWebsite/settings.py
@@ -59,7 +59,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = True
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
 
-ALLOWED_HOSTS = ['agusancolleges.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 # ALLOWED_HOSTS = []
 
 
