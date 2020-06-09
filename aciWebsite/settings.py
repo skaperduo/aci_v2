f8cf9e6cdd2a163023862a11d7fe4afc9ba12d7f
Steven Chanjueco <hsb.chanjueco@gmail.com>
2020-06-09 21:39:04 +0800

    added TCP in programs

--- aciWebsite/settings.py
+++ aciWebsite/settings.py
@@ -59,8 +59,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = True
 DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
-ALLOWED_HOSTS = ['agusancolleges.herokuapp.com']
-# ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = ['agusancolleges.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
